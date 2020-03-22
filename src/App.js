904ca2309fb375869a4efd3a578fa1541e4c0a98
raymund.delacerna <rbdelacerna@gmail.com>
2020-03-22 23:53:43 +0800

    Added githubapps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CardList from "./components/cardlist";
+import Form from "./components/form";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import { Container, Row, Col } from "react-bootstrap";
+import { faCrown } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+library.add(fab, faCrown);
+
+class App extends Component {
+  state = {
+    profiles: []
+  };
+
+  addNewProfile = profileData => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData]
+    }));
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="wrapper">
+          <Header />
+          <main>
+            <Container>
+              <Row>
+                <Col>
+                  <div className="app-github-form">
+                    <Form onSubmit={this.addNewProfile} />
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div className="app-github-list">
+                    <CardList profiles={this.state.profiles} />
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </main>
+          <footer className="github-footer">
+            <Footer />
+          </footer>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
